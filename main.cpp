c60970dfe7d30749d84feaf2dc245d29a02fc547
Coding96 <Edward.Collins@live.co.uk>
2017-05-14 20:42:47 +0100

    more testing for Z layers

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
  * Created on 13 May 2017, 16:22
  */
 #include <GL/glut.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
 #include <cstdlib>
 #include <iostream>
 
@@ -24,37 +26,39 @@ int pause = 0;
 
 int main(int argc, char** argv)
 {
-    
-  glutInit (&argc, argv);
-  glutInitDisplayMode (GLUT_DOUBLE);
-  glutInitWindowSize(500,500);
-  glutInitWindowPosition(100,100);
-  
-  glutCreateWindow ("VR Testing");
-  init ();
-  glutDisplayFunc (display); 
-  glutReshapeFunc (reshape);
-  glutKeyboardFunc (keyboard);
-  glutIdleFunc (animate);
-  glutMainLoop ();
-    
-  return EXIT_SUCCESS;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+
+    glutCreateWindow("VR Testing");
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutIdleFunc(animate);
+    glutMainLoop();
+
+    return EXIT_SUCCESS;
 }
 
 void display(void)
 {
     glClearColor(0.0, 0.0, 0.0, 0.0);
-    glClear (GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt (0.0,0.0,5.0, 0.0,0.0,0.0, 0.0,1.0,0.0);
-        glRotatef(angle,1,0,0);
-        glRotatef(angle,0,1,0);
-        glRotatef(angle,0,0,1);
-        glColor3f(colour1,colour2,colour3);
-        glutWireCube(2.0);
+    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glRotatef(angle, 1, 0, 0);
+    glRotatef(angle, 0, 1, 0);
+    glRotatef(angle, 0, 0, 1);
+    glColor3f(colour1, colour2, colour3);
+    glutWireCube(2.0);
 
-    
-    glutSwapBuffers();    
+    glColor3f(colour2, colour1, colour3);
+    glutSolidSphere(0.5,20,20);
+
+    glutSwapBuffers();
 }
 
 void init(void)
@@ -66,33 +70,33 @@ void init(void)
 void animate(void)
 {
     //works an idle function
-    
-    if(pause == 0)
-    {
-        angle += 0.7;
 
-        if(colour1 < 1)
+    if (pause == 0)
+    {
+        angle += 0.8;
+
+        if (colour1 < 1)
         {
-            colour1 += 0.02;
+            colour1 += 0.007;
         }
         else
-        { 
+        {
             colour1 = 0;
         }
-        if(colour2 < 1)
+        if (colour2 < 1)
         {
-            colour2 += 0.04;
+            colour2 += 0.0211;
         }
         else
-        { 
+        {
             colour2 = 0;
         }
-        if(colour3 < 1)
+        if (colour3 < 1)
         {
-            colour3 += 0.06;
+            colour3 += 0.0288;
         }
         else
-        { 
+        {
             colour3 = 0;
         }
     }
@@ -102,25 +106,26 @@ void animate(void)
 void keyboard(unsigned char key, int x, int y)
 {
     using std::cout;
-    
-  switch (key)
-  {
-    case 27:  /* Escape key */
-      exit(0);
+
+    switch (key)
+    {
+    case 27: /* Escape key */
+        exit(0);
     case 112:
         pause = !pause;
         break;
     default:
-      cout << "You pressed " << key << "\n";
-  }
-} 
+        cout << "You pressed " << key << "\n";
+        break;
+    }
+}
 
-void reshape (int w,int h)
+void reshape(int w, int h)
 {
     /*called when windows is moved or resized etc..*/
-    glViewport (0,0, (GLsizei) w, (GLsizei) h);
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective (60, (GLfloat)w/(GLfloat)h, 1.0, 100.0);
-    glMatrixMode (GL_MODELVIEW);
+    gluPerspective(60, (GLfloat) w / (GLfloat) h, 1.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
 }
