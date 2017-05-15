9f1393c8bf0a7a4fa9f3c6f31b10a60808bda15a
Coding96 <Edward.Collins@live.co.uk>
2017-05-15 17:03:05 +0100

    added the loader for a cube with 8 vertex

--- AssetLoader.cpp
+++ AssetLoader.cpp
@@ -2,12 +2,14 @@
 //asset loader loads premade objects into the frame
 #include "AssetLoader.h"
 
-void cubeloader(void)
+cube* cubeloader(void)
 {
+    //creates a pointer to a new cube
+    cube* newcube = (cube*) malloc(sizeof(cube));
     
+    return newcube;
 }
 
-void sphereloader(void)
-{
+void sphereloader(void){
     
 }
--- AssetLoader.h
+++ AssetLoader.h
@@ -1,5 +1,19 @@
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
 
-extern void cubeloader();
-extern void sphereloader();
+typedef struct{
+    GLfloat vertex1;
+    GLfloat vertex2;
+    GLfloat vertex3;
+    GLfloat vertex4;
+    GLfloat vertex5;
+    GLfloat vertex6;
+    GLfloat vertex7;
+    GLfloat vertex8;
+}cube;
+
+extern cube* cubeloader(void);
+extern void sphereloader(void);
 
 
